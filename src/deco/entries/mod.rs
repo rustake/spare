1db60dd49963623f28da54fd9b6cda57af425afd
Hoyeung <hoyeungw@outlook.com>
2021-12-03 17:34:00 +0800

    add Decorable trait for entries

--- src/deco/entries/mod.rs
+++ src/deco/entries/mod.rs
@@ -1,24 +1,65 @@
-use std::fmt;
+use std::fmt::Display;
 
-use palett::fluo::entries::fluo_rendered;
-use palett::presets::{OCEAN, PLANET};
-use veho::vector::mapper;
+use palett::fluo::vector::fluo_rendered;
+use palett::types::Preset;
+use veho::entries::unwind;
+use veho::vector::{Mappers, zipper};
 
-pub fn deco_entries<K, V, KVS>(entries: KVS, de: &str) -> String where
-    K: fmt::Display,
-    V: fmt::Display,
-    KVS: IntoIterator<Item=(K, V)>
+use crate::padder::vector_padder;
+
+pub trait Decorable<K, V>: IntoIterator<Item=(K, V)> where
+    K: Display,
+    V: Display,
+    Self: Sized
 {
-    let texts = fluo_rendered(entries, &(OCEAN, PLANET), &[]);
-    let lines: Vec<String> = mapper(texts, |(k, v)| format!("  {}{}{},", k, de, v));
-    return format!("{{\n{}\n}}", lines.join("\n"));
+    fn deco_entries(self,
+                    de: Option<&str>,
+                    presets: Option<&(Preset, Preset)>, ) -> String where
+    {
+        let delim = match de { None => ": ", Some(tx) => tx, };
+        let (keys, items) = unwind(self);
+        let (keys, items) = match presets {
+            None => (
+                keys.mapper(|x| x.to_string()),
+                items.mapper(|x| x.to_string())
+            ),
+            Some(p) => (
+                fluo_rendered(keys, p, &[]),
+                fluo_rendered(items, p, &[])
+            ),
+        };
+        let (keys, items) = (
+            vector_padder(keys, true),
+            vector_padder(items, true)
+        );
+
+        let lines: Vec<String> = zipper(keys, items, |k, v| format!("  {}{}{},", k, delim, v));
+        return format!("{{\n{}\n}}", lines.join("\n"));
+    }
 }
+
+impl<K, V, KVS> Decorable<K, V> for KVS where
+    K: Display,
+    V: Display,
+    KVS: IntoIterator<Item=(K, V)>
+{}
+
+pub fn deco_entries<K, V, KVS>(entries: KVS,
+                               de: Option<&str>,
+                               presets: Option<&(Preset, Preset)>, ) -> String where
+    K: Display,
+    V: Display,
+    KVS: IntoIterator<Item=(K, V)>
+{ entries.deco_entries(de, presets) }
 
 #[cfg(test)]
 mod deco_entries_tests {
+    use palett::presets::{METRO, OCEAN};
     use veho::entries::IntoHashmap;
 
-    use crate::deco_entries;
+    use crate::deco::entries::Decorable;
+
+// use crate::deco_entries;
 
     #[test]
     fn test() {
@@ -30,7 +71,9 @@ mod deco_entries_tests {
             ("cinq", "5"),
             ("six", "6"),
         ].into_hashmap();
-        let texted_mx = deco_entries(entries, ": ");
-        println!("entries = {}", texted_mx);
+        let texted_mx = (&entries).deco_entries(Some(": "), Some(&(OCEAN, METRO)));
+        println!(">> [entries] {}", texted_mx);
+        println!(">> [len] {}", entries.len());
+        println!(">> [capacity] {}", entries.capacity());
     }
 }
