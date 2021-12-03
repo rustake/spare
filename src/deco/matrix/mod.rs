3995ef2fe42eb685921b1ee19df629c16d120683
Hoyeung <hoyeungw@outlook.com>
2021-12-03 17:33:47 +0800

    corrected ansi feature for deco_matrix

--- src/deco/matrix/mod.rs
+++ src/deco/matrix/mod.rs
@@ -1,4 +1,4 @@
-use std::fmt;
+use std::fmt::Display;
 
 use palett::fluo::matrix::fluo_rendered;
 use palett::types::Preset;
@@ -7,26 +7,40 @@ use veho::vector::mapper;
 
 use crate::padder::matrix_padder;
 
-pub fn deco_matrix<R, M>(mx: M,
-                         de: Option<&str>,
-                         presets: Option<(Preset, Preset)>,
-) -> String where
+pub trait Decorable<R>: IntoIterator<Item=R> where
+    R: IntoIterator,
+    R::Item: Display,
+    Self: Sized,
+{
+    fn deco_matrix(self, de: Option<&str>, presets: Option<&(Preset, Preset)>) -> String {
+        let delim = match de {
+            None => ", ",
+            Some(tx) => tx,
+        };
+        let texts = match presets {
+            None => self.mapper(|x| x.to_string()),
+            Some(p) => fluo_rendered(self, p, &[]),
+        };
+        let padded = matrix_padder(texts, true);
+        let lines: Vec<String> = mapper(padded, |vec| format!("  [ {} ],", vec.join(delim)));
+        return format!("[\n{}\n]", lines.join("\n"));
+    }
+}
+
+impl<M, R> Decorable<R> for M where
     M: IntoIterator<Item=R>,
     R: IntoIterator,
-    R::Item: fmt::Display
+    R::Item: Display
+{}
+
+pub fn deco_matrix<R, M>(mx: M,
+                         de: Option<&str>,
+                         presets: Option<&(Preset, Preset)>, ) -> String where
+    M: IntoIterator<Item=R>,
+    R: IntoIterator,
+    R::Item: Display
 {
-    let de = match de {
-        None => ", ",
-        Some(tx) => tx,
-    };
-    let texts = match presets {
-        None => mx.mapper(|el| el.to_string()),
-        Some(pr) => fluo_rendered(mx, &pr, &[]),
-    };
-    // let texts = fluo_rendered(mx, &(OCEAN, PLANET), &[]);
-    let padded = matrix_padder(texts, true);
-    let lines: Vec<String> = mapper(padded, |vec| format!("  [ {} ],", vec.join(de)));
-    return format!("[\n{}\n]", lines.join("\n"));
+    mx.deco_matrix(de, presets)
 }
 
 // pub macro_rules! deco_matrix {
@@ -46,9 +60,9 @@ mod deco_matrix_tests {
     fn test() {
         let matrix = init(5, 5, |i, j| ((i as i32) + 1) * 10_i32.pow(j as u32));
         let matrix = matrix.transpose();
-        let texted = deco_matrix(&matrix, None, Some((OCEAN, PLANET)));
+        let texted = deco_matrix(&matrix, None, Some(&(OCEAN, PLANET)));
         println!(">> matrix = {}", texted);
-        let texted = deco_matrix(&matrix, None, Some((POME, METRO)));
+        let texted = deco_matrix(&matrix, None, Some(&(POME, METRO)));
         println!(">> matrix = {}", texted);
     }
 }
