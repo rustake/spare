bf87b302231665df97641786edee9edfea541a54
Hoyeung <hoyeungw@outlook.com>
2021-12-03 11:42:54 +0800

    add de and presets to deco_vector

--- src/deco/vector/mod.rs
+++ src/deco/vector/mod.rs
@@ -1,30 +1,11 @@
 use std::fmt;
-use std::fmt::Write;
 
 use palett::fluo::vector::fluo_rendered;
 use palett::presets::{FRESH, OCEAN};
+use palett::types::Preset;
 use veho::vector::Mappers;
 
-pub trait Itertools: Iterator {
-    fn join(&mut self, delim: &str) -> String
-        where Self::Item: fmt::Display
-    {
-        match self.next() {
-            None => String::new(),
-            Some(first) => {
-                // estimate lower bound of capacity needed
-                let (lower, _) = self.size_hint();
-                let mut result = String::with_capacity(delim.len() * lower);
-                write!(&mut result, "{}", first).unwrap();
-                self.for_each(|element| {
-                    result.push_str(delim);
-                    write!(&mut result, "{}", element).unwrap();
-                });
-                result
-            }
-        }
-    }
-}
+mod joiners;
 
 trait Decorable: IntoIterator {
     fn deco(self, de: &str) -> String where
@@ -47,22 +28,34 @@ impl<I> Decorable for I where
 
 
 // #[cfg(feature = "use_alloc")]
-pub fn deco_vector<I>(it: I, de: &str) -> String where
+pub fn deco_vector<I>(vec: I,
+                      de: Option<&str>,
+                      presets: Option<&(Preset, Preset)>, ) -> String where
     I: IntoIterator,
     I::Item: fmt::Display
 {
-    it.deco(de)
+    let de = match de { None => ", ", Some(tx) => tx, };
+    let texts = match presets {
+        None => vec.mapper(|x| x.to_string()),
+        Some(p) => fluo_rendered(vec, p, &[]),
+    };
+    return format!("[ {} ]", texts.join(de));
+    vec.deco(de)
 }
 
 #[cfg(test)]
 mod tests {
+    use palett::presets::{FRESH, OCEAN};
+
     use crate::deco_vector;
 
     #[test]
     fn test() {
         let words = vec!["a", "b", "c", "-", "1", "2", "3"];
         words.join(", ");
-        let rendered = deco_vector(&words, ", ");
-        println!("{}", rendered);
+        let rendered = deco_vector(&words, Some(", "), Some(&(OCEAN, FRESH)));
+        println!(">> [vector] {}", rendered);
+        println!(">> [len] {}", words.len());
+        println!(">> [capacity] {}", words.capacity())
     }
 }
