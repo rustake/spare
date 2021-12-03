e10ec5dd35b480c40889a51b56d3415ebcc43334
Hoyeung <hoyeungw@outlook.com>
2021-12-03 08:46:30 +0800

    add vector_padder

--- src/padder/matrix.rs
+++ src/padder/matrix.rs
@@ -1,11 +1,18 @@
 use aryth::indicator::vector::Indicators;
 use veho::columns::mapper as map_columns;
 use veho::matrix::{Mappers, Matrix};
+use veho::vector::mapper as map_vector;
 
-pub fn matrix_padder(mx: Matrix<String>) -> Matrix<String> where
+pub fn matrix_padder(mx: Matrix<String>) -> Matrix<String>
 {
     let widths = map_columns(&mx, |col| col.max_by(|x| x.len()).unwrap_or(0));
-    mx.indexed_mapper(|_, j, x| format!("{: >w$}", x, w = widths[j]))
+    return mx.indexed_mapper(|_, j, x| format!("{: >w$}", x, w = widths[j]));
+}
+
+pub fn vector_padder(vec: Vec<String>) -> Vec<String>
+{
+    let width = (&vec).max_by(|x| x.len()).unwrap_or(0);
+    return map_vector(vec, |x| format!("{: >w$}", x, w = width));
 }
 
 #[cfg(test)]
